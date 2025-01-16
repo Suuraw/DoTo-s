d86a8eca40400e37e5bcda49cb381c276b488e48
Sujay kumar <sujayraw13@gmail.com>
2025-01-16 20:57:18 +0530

    make app.jsx github pages compatible

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import {Route, Routes} from 'react-router';
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import ToDoList from './pages/ToDo/ToDoList';
 import './App.css';
 import 'antd/dist/reset.css';
+
 function App() {
   return (
-   <Routes>
+    <Router basename="/DoTo-s">
+      <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/to-do-list" element={<ToDoList />} />
-    </Routes>
-
-  )
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
